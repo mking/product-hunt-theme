4ce02dfc6c7a26068f9c8921745f08ddea147f14
Matthew King <mking@users.noreply.github.com>
2016-05-21 19:38:43 -0700

    master check

--- scripts/deploy-github.sh
+++ scripts/deploy-github.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+[ 'master' = $(git rev-parse --abbrev-ref HEAD) ] || echo 'Must be on master'
+
 git checkout gh-pages
+git reset --hard master
 npm run clean
 npm run build:github
 git add -f build
-git commit -am 'update build'
-git push
+git commit -m 'update build'
+git push -f
 git checkout master
